8e762635d10d0c2a03fe65684b65e1cdf0d6e519
Rishav-Raj-Sinha <134898640+Rishav-Raj-Sinha@users.noreply.github.com>
2023-10-10 18:55:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,10 @@ st.header(":3")
 
 st.image("7a1f6135d4e10c92dd51ab5531a7c8f0.jpg")
 
-def load_lottieurl(url: str):
+def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
-    
     return r.json()
 
 lottie_hello = load_lottieurl("https://lottie.host/15ddaaf6-71a9-4458-9f21-31e965ded5ac/49cQNEerWz.json")
