5321d60a3c9bd73370302663f9cb4cf166c482ee
Rishav-Raj-Sinha <134898640+Rishav-Raj-Sinha@users.noreply.github.com>
2023-10-10 18:05:07 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
+
+st.title("Hello Kitty")
+st.header(":3")
+
+st.image("7a1f6135d4e10c92dd51ab5531a7c8f0.jpg")
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    
+    return r.json()
+
+lottie_hello = load_lottieurl("https://lottie.host/15ddaaf6-71a9-4458-9f21-31e965ded5ac/49cQNEerWz.json")\
+
+st_lottie(lottie_hello, key="hello")
