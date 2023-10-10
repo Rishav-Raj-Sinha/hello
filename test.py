f6606ea4c7d1540b8becc7b1175c41d804cc181a
Rishav-Raj-Sinha <134898640+Rishav-Raj-Sinha@users.noreply.github.com>
2023-10-10 18:57:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,12 +8,12 @@ st.header(":3")
 
 st.image("7a1f6135d4e10c92dd51ab5531a7c8f0.jpg")
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+#def load_lottieurl(url):
+ #   r = requests.get(url)
+  #  if r.status_code != 200:
+   #     return None
+   # return r.json()
 
-lottie_hello = load_lottieurl("https://lottie.host/15ddaaf6-71a9-4458-9f21-31e965ded5ac/49cQNEerWz.json")
+#lottie_hello = load_lottieurl("https://lottie.host/15ddaaf6-71a9-4458-9f21-31e965ded5ac/49cQNEerWz.json")
 
-st_lottie(lottie_hello, key="hello")
+#st_lottie(lottie_hello, key="hello")
